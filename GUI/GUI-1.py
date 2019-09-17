1970961a0bf7a9b2203797949b41baf31e989d07
Mahmoud Amr <38290949+mahmoudamr1205@users.noreply.github.com>
2019-09-17 18:00:33 +0200

    Update GUI-1.py

--- GUI/GUI-1.py
+++ GUI/GUI-1.py
@@ -1,28 +1,55 @@
 from guizero import App, Text, TextBox, PushButton, Slider, Picture, Window
 
 import tkinter as tk
+import serial
+import time
+import os
 
 
-#import RPi.GPIO as GPIO
+arduino = serial.Serial("COM8",9600)
 
-# arabic or english
+
+
 
 def select_3():
-    print("ابو 3")
-    return
+        flag3 = 0
+        if flag3 == 0 :
+            #os.system('notepad /p C:\\Users\\mahmo\\OneDrive\\Desktop\\3.txt')
+            time.sleep(0.1) 
+            arduino.write(b'3')
+            os.system('notepad /p 3.txt')
+
+            flag3 = 1
+        return
   
 def select_5():
-    print(" واحده من ابو  5")
-    return
+        flag5 = 0
+        if flag5 == 0 :
+            time.sleep(0.1) 
+            arduino.write(b'5')
+            os.system('notepad /p 5.txt')
+            flag5 = 1
+        return
 
 
 def select_7():
-    print(" ابو 7")
-    return
+        flag7 = 0
+        if flag7 == 0 :
+            time.sleep(0.1) 
+            arduino.write(b'7')    
+            os.system('notepad /p 7.txt')
+            flag7 = 1
+        return
     
 def select_10():
-    print("ابو 10 و هات الباقى")
-    return 
+        
+        flag10 = 0
+        if flag10 == 0 :
+            time.sleep(0.1) 
+            arduino.write(b'9')
+            os.system('notepad /p ten.txt')
+            flag10 = 1
+        return 
 
 
 def open_window():
@@ -30,12 +57,10 @@ def open_window():
 
 def close_window():
     window.hide()
-
-
-
+ 
     
 app = App(title="مترو")
-#app = App(layout="grid")
+    #app = App(layout="grid")
 
 
 window = Window(app, title="Map")
@@ -44,16 +69,16 @@ window.hide()
 
 
 maintext = Text(app, text="نوع التذكره"
-                       ,size=60
-                       ,font="Times new roman"
-                       ,color="red")
+                        ,size=60
+                        ,font="Times new roman"
+                        ,color="red")
 
-open_button = PushButton(app, command=open_window ,pady=50, padx=100 , text="Show Map")
+open_button = PushButton(app, command=open_window ,pady=80, padx=150 , text="Show Map")
 
-x_3= PushButton(app,command=select_3 ,pady=50, padx=100,text=" 3 جنيه",align="left")
-x_5 = PushButton(app,command=select_5 ,pady=50,padx=100,text=" 5 جنيه",align="left")
-x_7 = PushButton(app,command=select_7 ,pady=50,padx=100,text=" 7 جنيه",align="left")
-x_10= PushButton(app,command=select_10 ,pady=50, padx=100,text="10 جنيه",align="left")
+x_3= PushButton(app,command=select_3 ,pady=50, padx=120,text=" 3 جنيه",align="left")
+x_5 = PushButton(app,command=select_5 ,pady=50,padx=120,text=" 5 جنيه",align="left")
+x_7 = PushButton(app,command=select_7 ,pady=50,padx=120,text=" 7 جنيه",align="left")
+x_10= PushButton(app,command=select_10 ,pady=50, padx=120,text="10 جنيه",align="left")
 
 close_button = PushButton(window, command=close_window ,pady=50, padx=100, text="Choose ticket" ,align="left")
 
@@ -61,6 +86,6 @@ picture = Picture(window,  width=700, height=950, image="map.png",align="right")
 
 
 
-
-
 app.display()
+
+
